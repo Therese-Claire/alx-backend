f5cec0be02328c5ddc3b0ab087fb8aa71dc9a6c6
Therese-Claire <awonguagabi@gmail.com>
2024-08-03 01:22:41 +0100

    corrected typing error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-"""basic babel setup in the flask app"""
-
+"""sets up a simple flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 
 
 class Config:
-    """language config for our app"""
-    LANGUAGES = ["en", "fr"]
+    """config for simple flask app"""
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app.config.from_object(Config)
+babel = Babel(app)
+
+
 @app.route('/')
 def index():
-    """default route"""
-    return render_template('1-index.html')
+    """index route"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
