86fc75cf73655fa7fcd37f337a1fad04596e29f1
Therese-Claire <awonguagabi@gmail.com>
2024-08-03 01:17:07 +0100

    corrected typing error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """basic babel setup in the flask app"""
 
-from flask import Flask, rebder_template
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
